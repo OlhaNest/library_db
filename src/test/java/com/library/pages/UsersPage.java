2c3467e41eb231a9901a84d11d7dad3b8d2c9a21
Ankit Malla <ankit93malla@gmail.com>
2022-12-03 18:15:22 -0500

    Ankit Feature created

--- src/test/java/com/library/pages/UsersPage.java
+++ src/test/java/com/library/pages/UsersPage.java
@@ -1,6 +1,6 @@
 package com.library.pages;
 
-import com.cydeo.utility.Driver;
+import com.library.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
