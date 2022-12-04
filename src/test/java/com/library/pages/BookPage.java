6426ebcb0de9b6d7dd39b4a7debbc2d33e3839e7
Luis Valadez <valopezla2sc@gmail.com>
2022-12-04 05:34:34 -0600

    almost done

--- src/test/java/com/library/pages/BookPage.java
+++ src/test/java/com/library/pages/BookPage.java
@@ -5,10 +5,15 @@ import com.library.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
-public class BookPage extends BasePage {
+public class BookPage {
+
+    public BookPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 
     @FindBy(xpath = "//table/tbody/tr")
     public List<WebElement> allRows;
@@ -19,24 +24,34 @@ public class BookPage extends BasePage {
     @FindBy(id = "book_categories")
     public WebElement mainCategoryElement;
 
-    @FindBy(name = "name")
+    @FindBy(xpath = "//input[@name='name']")
     public WebElement bookName;
 
 
-    @FindBy(xpath = "(//input[@type='text'])[4]")
+    @FindBy(xpath = "//input[@name='author']")
     public WebElement author;
 
 
-    @FindBy(name = "year")
+    @FindBy(xpath = "//input[@name='year']")
     public WebElement year;
 
-    @FindBy(name = "isbn")
+    @FindBy(xpath = "//input[@name='isbn']")
     public WebElement isbn;
 
     @FindBy(id = "description")
     public WebElement description;
 
+    @FindBy(xpath = "//i[@class='fa fa-plus']" )
+    public WebElement addBook;
 
+    @FindBy(xpath = "//select[@id='book_group_id']")
+    public WebElement selectCategory;
+
+@FindBy(xpath = "//button[.='Save changes']")
+public WebElement saveChanges;
+
+@FindBy(xpath = "//table[@id='tbl_books']//tr[1]//td[3]")
+public WebElement bookTextFirstRow;
 
     public WebElement editBook(String book) {
         String xpath = "//td[3][.='" + book + "']/../td/a";
--- src/test/java/com/library/step_defs/AddedBooks_StepDefs.java
+++ src/test/java/com/library/step_defs/AddedBooks_StepDefs.java
@@ -1,71 +1,77 @@
 package com.library.step_defs;
 
 import com.library.pages.BasePage;
+import com.library.pages.BookPage;
 import com.library.pages.LoginPage;
+import com.library.utilities.BrowserUtil;
 import com.library.utilities.ConfigurationReader;
 import com.library.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 public class AddedBooks_StepDefs {
     LoginPage login1 = new LoginPage();
-
+    BasePage base1 = new BasePage();
+BookPage bookPage = new BookPage();
 
     @Given("I login as a librarian")
     public void i_login_as_a_librarian() {
         Driver.getDriver().get(ConfigurationReader.getProperty("library_url"));
         login1.login(ConfigurationReader.getProperty("luis_username"), ConfigurationReader.getProperty("luis_password") );
     }
-
     @Given("I navigate to {string} page")
     public void i_navigate_to_page(String string) {
-
+base1.books.click();
     }
     @When("the librarian click to add book")
     public void the_librarian_click_to_add_book() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+bookPage.addBook.click();
     }
     @When("the librarian enter book name {string}")
-    public void the_librarian_enter_book_name(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void the_librarian_enter_book_name(String bookName1) {
+        bookPage.bookName.sendKeys(bookName1);
     }
     @When("the librarian enter ISBN {string}")
-    public void the_librarian_enter_isbn(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void the_librarian_enter_isbn(String isbn1) {
+        bookPage.isbn.sendKeys(isbn1);
     }
     @When("the librarian enter year {string}")
-    public void the_librarian_enter_year(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void the_librarian_enter_year(String year1) {
+        bookPage.year.sendKeys(year1);
     }
     @When("the librarian enter author {string}")
-    public void the_librarian_enter_author(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void the_librarian_enter_author(String author1) {
+        bookPage.author.sendKeys(author1);
     }
     @When("the librarian choose the book category {string}")
-    public void the_librarian_choose_the_book_category(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void the_librarian_choose_the_book_category(String category) {
+        Select select = new Select(bookPage.selectCategory);
+        select.selectByVisibleText(category);
     }
     @When("the librarian click to save changes")
     public void the_librarian_click_to_save_changes() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+bookPage.saveChanges.click();
     }
+
     @Then("the librarian verify new book by {string}")
-    public void the_librarian_verify_new_book_by(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void the_librarian_verify_new_book_by(String expected) {
+        BrowserUtil.waitFor(2);
+        bookPage.search.sendKeys(expected);
+        BrowserUtil.waitFor(1);
+        String actualBookName = bookPage.bookTextFirstRow.getText();
+        Assert.assertEquals(expected, actualBookName);
     }
-    @Then("the librarian verify new book from database by {string}")
+
+   @Then("the librarian verify new book from database by {string}")
     public void the_librarian_verify_new_book_from_database_by(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+
     }
+
+
 
 }
